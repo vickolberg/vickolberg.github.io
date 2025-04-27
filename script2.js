b2e0a1c359155ee00d4c0490d2f54f9cbb278240
Victoria Kolberg <vickolberg@gmail.com>
2025-04-27 18:21:08 -0400

    cleaned up the js and updated dropdown

--- script2.js
+++ script2.js
@@ -7,7 +7,7 @@ let secretWordArray = secretWord.split("");
 
 console.log(secretWordArray);
 
-// image files stored in an array
+// image files stored in an array - drew them on the ipad
 const imageFiles = [
     "/hangmanImages/image2.png",
     "/hangmanImages/image3.png",
@@ -20,13 +20,10 @@ const imageFiles = [
     "/hangmanImages/image10.png"
 ];
 
-// Function to change the image
-//no touch. works well.
 function changeImage() {
     image.setAttribute('src', imageFiles[currentIndex]);
     currentIndex++;
 
-    // Check if all images have been shown
     if (currentIndex > 8) {
         console.log(currentIndex);
         document.getElementById("result").innerText = "You lost!";
@@ -35,15 +32,12 @@ function changeImage() {
         giveAway.textContent = `The word was: ${secretWord}`;
         document.body.appendChild(giveAway);
 
-        // Cancel event listener when player loses
         document.querySelectorAll('.alphabetLetter').forEach(button => {
             button.removeEventListener("click", handleButtonClick);
         });
     }
 }
 
-// Function to generate styled underlines 
-// no touch. works well.
 function generateStyledUnderlines(secretWord) {
     let underlines = '';
     for (let letter of secretWord) {
@@ -56,12 +50,10 @@ function generateStyledUnderlines(secretWord) {
     return underlines;
 }
 
-//no touch. works well
 function revealLetter(element, letter) {
     element.textContent = letter;
 }
 
-// no touch. works well.
 function revealLetterInSecretWord(clickedLetter) {
     let indices = [];
     for (let i = 0; i < secretWordArray.length; i++) {
@@ -76,7 +68,6 @@ function revealLetterInSecretWord(clickedLetter) {
             revealLetter(elements[index], secretWordArray[index]);
         }
 
-        // Check if all letters have been revealed
         let allRevealed = true;
         for (let i = 0; i < secretWordArray.length; i++) {
             if (elements[i].textContent !== secretWordArray[i]) {
@@ -90,7 +81,7 @@ function revealLetterInSecretWord(clickedLetter) {
             for (let i = 0; i < alphabetLetters.length; i++) {
                 alphabetLetters[i].style.cursor = "not-allowed";
             }
-            // Remove event listener from all buttons
+
             for (let i = 0; i < alphabetLetters.length; i++) {
                 alphabetLetters[i].removeEventListener("click", handleButtonClick);
             }
@@ -103,46 +94,34 @@ function revealLetterInSecretWord(clickedLetter) {
     }
 }
 
-// Function to handle button click
 function handleButtonClick() {
     clickedLetter = this.innerText;
-    // Change style after button has been clicked
     this.style.backgroundColor = "#4f473f";
     this.style.textDecoration = "line-through";
-    console.log(clickedLetter); // For debugging it will display the letter clicked
+    console.log(clickedLetter); 
     revealLetterInSecretWord(clickedLetter);
 
-    // Check if clicked letter is not in the secret word
     if (!secretWordArray.includes(clickedLetter.toLowerCase()) && 
         !secretWordArray.includes(clickedLetter.toUpperCase())) {
         changeImage();
     }
 }
 
-// Create div element with id
-// no touch. works well
+
 const buttonsDiv = document.createElement('div');
 buttonsDiv.id = 'buttonsDiv';
 
-// Create button group inside buttonsDiv with 26 buttons
-// no touch. works well.
 for (let i = 0; i < 26; i++) {
-    const button = document.createElement('button'); // Create a button element
-    button.className = 'alphabetLetter'; // Assign button class
-    // Set the inner text of the button to a letter of the alphabet
+    const button = document.createElement('button'); 
+    button.className = 'alphabetLetter'; 
     button.innerText = String.fromCharCode(65 + i); // ASCII code for 'A' is 65
-    // Append the button to the buttonsDiv
     buttonsDiv.appendChild(button);
-    // Add event listener to each button
     button.addEventListener("click", handleButtonClick);
 }
-// Append the buttonsDiv to the document body
-// no touch. works well.
+
 document.body.appendChild(buttonsDiv);
 
-// Generate and display underlines for secretWord
-//no touch. works well.
+
 let styledUnderlines = generateStyledUnderlines(secretWord);
-// Append underlines to an existing element with id "blanks"
-//no touch. works well.
+
 document.getElementById("blanks").innerHTML = styledUnderlines;
